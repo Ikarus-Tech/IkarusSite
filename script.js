56776d08970f3feae3597412ed77a83b63a0b5ad
Felizardo Muchanga <felizardopaulo40@gmail.com>
2024-10-25 11:31:23 +0200

    Responsividade & Melhoria de Interactividade

--- script.js
+++ script.js
@@ -26,6 +26,24 @@ const showMenu = (toggleId, navId) =>{
         // Add show-icon to show and hide the menu icon
         toggle.classList.toggle('show-icon')
     })
- }
+}
  
- showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle','nav-menu')
+
+// Seleciona os elementos #qos-item1 e #qos-item2
+const item1 = document.getElementById('qos-item1');
+const item2 = document.getElementById('qos-item2');
+
+// Adiciona o evento de mouseover para o item 2
+item2.addEventListener('mouseover', function() {
+    // Diminui a opacidade do item 1 quando o mouse estiver sobre o item 2
+    item1.style.opacity = '0.4';
+});
+
+// Adiciona o evento de mouseout para restaurar a opacidade do item 1
+item2.addEventListener('mouseout', function() {
+    // Restaura a opacidade original do item 1 quando o mouse sair do item 2
+    item1.style.opacity = '1';
+});
+
+
